a9166ca633da9ec788076d774d15fb609786e4f2
dani3l8200 <dani3l25948@gmail.com>
2020-08-20 16:36:51 -0600

    Menu Principales, se esta trabajando para que reconozca los comandos

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ import "MIA-PROYECTO1/LWH"
 func main() {
 	LWH.Execute()
 }
+
+/*if x.value == "mkdisk" {
+	fmt.Println(x.value)
+	for i, s := range x.children {
+
+		fmt.Println(i, s.value)
+	}
+} else if x.value == "exec" {
+	fmt.Println(x.value)
+	for k, m := range x.children {
+		fmt.Println(k, m.value)
+	}
+}*/
