c7c735e58ace6c8fe7694da4fb2da60da90a2463
dani3l8200 <dani3l25948@gmail.com>
2020-08-24 18:51:00 -0600

    add main

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
-import "MIA-PROYECTO1/LWH"
+import "MIA-PROYECTO1/analyzers"
 
 func main() {
-	LWH.Execute()
+	analyzers.Execute()
+	/*if _, err := os.Stat("/home/dani3l8200/gocode/src/intento/day.go"); err == nil {
+		fmt.Println("existe")
+
+	} else if os.IsNotExist(err) {
+		// path/to/whatever does *not* exist
+		log.Fatal(err)
+
+	} else {
+		// Schrodinger: file may or may not exist. See err for details.
+		fmt.Println("F")
+		// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
+
+	}*/
+
 }
 
 /*if x.value == "mkdisk" {
 	fmt.Println(x.value)
 	for i, s := range x.children {
-
 		fmt.Println(i, s.value)
 	}
 } else if x.value == "exec" {
@@ -17,4 +30,6 @@ func main() {
 	for k, m := range x.children {
 		fmt.Println(k, m.value)
 	}
+
+
 }*/
