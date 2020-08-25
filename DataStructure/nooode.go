0c6cc958cee4c7fad7e6c24ede0d9203988506e7
dani3l8200 <dani3l25948@gmail.com>
2020-08-24 18:47:40 -0600

    lista simple que se utilizara para los montajes del disco

---
+++ DataStructure/nooode.go
@@ -0,0 +1,26 @@
+package DataStructure
+
+/*type Node struct {
+	value interface{}
+	next  *Node
+}
+
+// Getters
+
+// Value method to return the current node value
+func (n *Node) Value() interface{} {
+	return n.value
+}
+
+// Next method to return the next node if exists
+func (n *Node) Next() *Node {
+	return n.next
+}
+
+// Setters
+
+// SetNext method to set the next node
+func (n *Node) SetNext(next *Node) {
+	n.next = next
+}
+*/
