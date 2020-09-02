ec4c2e5ff780e282f01eeb4328d4ecdd059d408e
dani3l8200 <dani3l25948@gmail.com>
2020-09-01 20:22:01 -0600

    se terminaron las particiones solo hace falta el delete y add

--- lwh/fdisk.go
+++ lwh/fdisk.go
@@ -15,67 +15,53 @@ import (
 func MakeFdisk(root Node) {
 	var path string = ""
 	var name string = ""
-	var size int32 = 0
+	var size int64 = 0
 	var unit byte = 'K'
 	var tipo byte = 'P'
-	var fit byte = 'W'
+	var fit byte = 'F'
 	var del int8 = -1
 	var add int16 = 0
 	for _, i := range root.Children {
 		if i.TypeToken == "SIZE" {
-			k, err := strconv.Atoi(i.Value)
+			k, err := strconv.ParseInt(i.Value, 10, 64)
 			if err != nil {
 				log.Fatal(err)
 			}
-			size = int32(k)
-			fmt.Println(size)
+			size = k
 		} else if i.TypeToken == "UNIT" {
 			if strings.EqualFold(i.Value, "k") {
 				unit = 'K'
-				fmt.Printf("%c\n", unit)
 			} else if strings.EqualFold(i.Value, "m") {
 				unit = 'M'
-				fmt.Printf("%c\n", unit)
 			} else if strings.EqualFold(i.Value, "b") {
 				unit = 'B'
-				fmt.Printf("%c\n", unit)
 			}
 		} else if i.TypeToken == "PATH" {
 			path = i.Value
-			fmt.Printf("%+v\n", path)
 		} else if i.TypeToken == "TYPE" {
 			if strings.EqualFold(i.Value, "p") {
 				tipo = 'P'
-				fmt.Printf("%c\n", tipo)
 			} else if strings.EqualFold(i.Value, "e") {
 				tipo = 'E'
-				fmt.Printf("%c\n", tipo)
 			} else if strings.EqualFold(i.Value, "l") {
 				tipo = 'L'
-				fmt.Printf("%c\n", tipo)
 			}
 		} else if i.TypeToken == "FIT" {
 			if strings.EqualFold(i.Value, "BF") {
 				fit = 'B'
-				fmt.Printf("%c\n", fit)
 			} else if strings.EqualFold(i.Value, "FF") {
 				fit = 'F'
-				fmt.Printf("%c\n", fit)
 			} else if strings.EqualFold(i.Value, "WF") {
 				unit = 'W'
-				fmt.Printf("%c\n", fit)
 			}
 		} else if i.TypeToken == "DELETE" {
 			if strings.EqualFold(i.Value, "fast") {
 				del = 0
-				fmt.Printf("%+v\n", del)
 			} else if strings.EqualFold(i.Value, "full") {
 				del = 1
-				fmt.Printf("%+v\n", del)
 			}
 		} else if i.TypeToken == "NAME" {
 			name = i.Value
-			fmt.Printf("%+v\n", name)
 		} else if i.TypeToken == "ADD" {
 			k, err := strconv.Atoi(i.Value)
 			if err != nil {
@@ -86,47 +72,36 @@ func MakeFdisk(root Node) {
 		}
 		for _, j := range i.Children {
 			if j.TypeToken == "SIZE" {
-				k, err := strconv.Atoi(j.Value)
+				k, err := strconv.ParseInt(j.Value, 10, 64)
 				if err != nil {
 					log.Fatal(err)
 				}
-				size = int32(k)
-				fmt.Println(size)
+				size = k
 			} else if j.TypeToken == "UNIT" {
 				if strings.EqualFold(j.Value, "k") {
 					unit = 'K'
-					fmt.Printf("%c\n", unit)
 				} else if strings.EqualFold(j.Value, "m") {
 					unit = 'M'
-					fmt.Printf("%c\n", unit)
 				} else if strings.EqualFold(j.Value, "b") {
 					unit = 'B'
-					fmt.Printf("%c\n", unit)
 				}
 			} else if j.TypeToken == "PATH" {
 				path = j.Value
-				fmt.Printf("%+v\n", path)
 			} else if j.TypeToken == "TYPE" {
 				if strings.EqualFold(j.Value, "p") {
 					tipo = 'P'
-					fmt.Printf("%c\n", tipo)
 				} else if strings.EqualFold(j.Value, "e") {
 					tipo = 'E'
-					fmt.Printf("%c\n", tipo)
 				} else if strings.EqualFold(j.Value, "l") {
 					tipo = 'L'
-					fmt.Printf("%c\n", tipo)
 				}
 			} else if j.TypeToken == "FIT" {
 				if strings.EqualFold(j.Value, "BF") {
 					fit = 'B'
-					fmt.Printf("%c\n", fit)
 				} else if strings.EqualFold(j.Value, "FF") {
 					fit = 'F'
-					fmt.Printf("%c\n", fit)
 				} else if strings.EqualFold(j.Value, "WF") {
 					unit = 'W'
-					fmt.Printf("%c\n", fit)
 				}
 			} else if j.TypeToken == "DELETE" {
 				if strings.EqualFold(j.Value, "fast") {
@@ -138,7 +113,6 @@ func MakeFdisk(root Node) {
 				}
 			} else if j.TypeToken == "NAME" {
 				name = j.Value
-				fmt.Printf("%+v\n", name)
 			} else if j.TypeToken == "ADD" {
 				k, err := strconv.Atoi(j.Value)
 				if err != nil {
@@ -154,10 +128,13 @@ func MakeFdisk(root Node) {
 		if size != 0 {
 			if tipo == 'P' {
 				ParticionPrimaria(path, name, tipo, fit, unit, size)
+				printMBR(path)
 			} else if tipo == 'E' {
 				ParticionExtendida(path, name, tipo, fit, unit, size)
+				printMBR(path)
 			} else if tipo == 'L' {
-
+				ParticionLogica(path, name, tipo, fit, unit, size)
+				printMBR(path)
 			}
 		} else if add != 0 {
 
@@ -171,7 +148,7 @@ func MakeFdisk(root Node) {
 }
 
 // ParticionPrimaria este metodo es usado para crear la particion primaria en el disco
-func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte, size int32) {
+func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte, size int64) {
 	f, err := os.OpenFile(path, os.O_RDWR, 0666)
 	if err != nil {
 		log.Fatal(err)
@@ -179,9 +156,11 @@ func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte,
 	defer f.Close()
 	auxSize := verifySize(unit, size)
 	auxName := converNameToByte(name)
-	fmt.Printf("%+v\n", auxSize)
 	m := readFileDisk(f, err)
-	var a int32 = 0
+	var ebr structs_lwh.EBR
+	//ReportMBR("/home/dani3l8200/Escritorio/MisDiscos/archivo.dot", m)
+
+	var a int64 = 0
 	for i := 0; i < 4; i++ {
 		if m.Partition[i].PartStatus != '1' {
 			a += m.Partition[i].PartSize
@@ -190,23 +169,24 @@ func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte,
 
 	if m.MbrSize-a >= auxSize {
 		var checkPartition bool = false
-		fmt.Println(checkPartition)
 		for i := 0; i < 4; i++ {
 			if m.Partition[i].PartName == auxName {
 				checkPartition = true
 				break
 			} else if m.Partition[i].PartType == 'E' {
-				f.Seek(int64(m.MbrSize), 0)
-				e := readFileEBR(f, err)
-				fmt.Println(e.PartStartE)
+				f.Seek(m.Partition[i].PartStart, 0)
+				ebr = readFileEBR(f, err)
+				tam := binary.Size(ebr)
 				pos, _ := f.Seek(0, os.SEEK_CUR)
 				fmt.Println(pos)
-				for pos < int64(m.Partition[i].PartSize)+int64(m.Partition[i].PartStart) {
-					if e.PartNameE == auxName {
+				for tam != 0 && pos < int64(m.Partition[i].PartSize)+int64(m.Partition[i].PartStart) {
+					if ebr.PartNameE == auxName {
 						checkPartition = true
-					}
-					if e.PartNextE == -1 {
+					} else if ebr.PartNextE == -1 {
 						break
+					} else {
+						f.Seek(ebr.PartNextE, 0)
+						ebr = readFileEBR(f, err)
 					}
 				}
 			}
@@ -232,12 +212,21 @@ func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte,
 				m.Partition[index].PartStatus = '0'
 
 				if index == 0 {
-					m.Partition[index].PartStart = int32(binary.Size(m))
+					m.Partition[index].PartStart = int64(binary.Size(m)) + 1
 				} else {
 					m.Partition[index].PartStart = m.Partition[index-1].PartStart + m.Partition[index-1].PartSize
 				}
-				writeInPrimaryPartition(path, m, index)
+
+				f.Seek(0, 0)
+
+				var binario bytes.Buffer
+
+				binary.Write(&binario, binary.BigEndian, &m)
+
+				writeNextBytes(f, binario.Bytes())
+
 				fmt.Println("SE CREO LA PARTICION PRIMARIA :D")
+
 			} else {
 				fmt.Println("YA SE HA CREADO 4 PARTICIONES YA NO SE PUEDEN CREAR MAS :(")
 			}
@@ -251,19 +240,17 @@ func ParticionPrimaria(path string, name string, tipo byte, fit byte, unit byte,
 }
 
 // ParticionExtendida Crea unicamente un particion extendida, escribiendo en el struct del mbr en su respetiva particion y en el EBR
-func ParticionExtendida(path string, name string, tipo byte, fit byte, unit byte, size int32) {
-	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+func ParticionExtendida(path string, name string, tipo byte, fit byte, unit byte, size int64) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 	auxSize := verifySize(unit, size)
 	auxName := converNameToByte(name)
-	fmt.Printf("%+v\n", auxSize)
 	m := readFileDisk(f, err)
-	e := readFileEBR(f, err)
 	var ebr structs_lwh.EBR
-	var a int32 = 0
+	var a int64 = 0
 
 	for i := 0; i < 4; i++ {
 		if m.Partition[i].PartType == 'E' {
@@ -285,8 +272,8 @@ func ParticionExtendida(path string, name string, tipo byte, fit byte, unit byte
 				checkPartition = true
 				break
 			} else if m.Partition[i].PartType == 'E' {
-				f.Seek(int64(m.MbrSize), int(m.Partition[i].PartStart))
 				pos, _ := f.Seek(0, os.SEEK_CUR)
+				e := readFileEBR(f, err)
 				eSize := binary.Size(e)
 				fmt.Println(pos)
 				for eSize != 0 && pos < int64(m.Partition[i].PartSize)+int64(m.Partition[i].PartStart) {
@@ -317,7 +304,7 @@ func ParticionExtendida(path string, name string, tipo byte, fit byte, unit byte
 				m.Partition[index].PartStatus = '0'
 
 				if index == 0 {
-					m.Partition[index].PartStart = int32(binary.Size(m))
+					m.Partition[index].PartStart = int64(binary.Size(m)) + 1
 				} else {
 					m.Partition[index].PartStart = m.Partition[index-1].PartStart + m.Partition[index-1].PartSize
 				}
@@ -342,8 +329,204 @@ func ParticionExtendida(path string, name string, tipo byte, fit byte, unit byte
 
 }
 
+func printMBR(path string) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Seek(0, 0)
+	m := readFileDisk(f, err)
+	var indexCheck int = 0
+	var fecha string = ""
+	var nameParticion string = ""
+	var exiteEBR bool = false
+	var e structs_lwh.EBR
+	fmt.Println("--------------------------------------MBR-------------------------------------")
+	fmt.Println("SIZE:", strconv.Itoa(int(m.MbrSize)))
+	fmt.Println("Signature:", strconv.Itoa(int(m.MbrDiskSignature)))
+	for _, k := range m.MbrTime {
+		if k != 0 {
+			fecha += string(k)
+		}
+	}
+	fmt.Println("FECHA:", fecha)
+	for i, mbr := range m.Partition {
+		fmt.Println("PARTCION NO.", strconv.Itoa(i+1))
+		fmt.Println("FIT:", string(mbr.PartFit))
+		for _, name := range mbr.PartName {
+			if name != 0 {
+				nameParticion += string(name)
+			}
+		}
+		fmt.Println("Nombre Particion:", nameParticion)
+		fmt.Println("Size de Particion:", strconv.Itoa(int(mbr.PartSize)))
+		fmt.Println("Start de paticion:", strconv.Itoa(int(mbr.PartStart)))
+		fmt.Println("Status de Particion:", string(mbr.PartStatus))
+		fmt.Println("Tipo de Particion:", string(mbr.PartType))
+		if mbr.PartType == 'E' {
+			indexCheck = i
+			exiteEBR = true
+		}
+		nameParticion = ""
+	}
+	x := 0
+	if exiteEBR {
+		f.Seek(m.Partition[indexCheck].PartStart, 0)
+		e = readFileEBR(f, err)
+		pos, _ := f.Seek(0, os.SEEK_CUR)
+		fmt.Println("---------------------EBR--------------------------------")
+		for pos < (m.Partition[indexCheck].PartStart + m.Partition[indexCheck].PartSize) {
+
+			if e.PartStatusE != '1' {
+				fmt.Println("PARTICION LOGICA NO.", strconv.Itoa(x+1))
+				fmt.Println("FIT_E:", string(e.PartFitE))
+				for _, k := range e.PartNameE {
+					if k != 0 {
+						nameParticion += string(k)
+					}
+				}
+				fmt.Println("NAME_E:", nameParticion)
+				fmt.Println("NEXT_E:", strconv.Itoa(int(e.PartNextE)))
+				fmt.Println("SIZE_E:", strconv.Itoa(int(e.PartSizeE)))
+				fmt.Println("START_E:", strconv.Itoa(int(e.PartStartE)))
+				fmt.Println("STATUS_E:", string(e.PartStatusE))
+				nameParticion = ""
+			}
+			if e.PartNextE == -1 {
+				break
+			} else {
+				f.Seek(e.PartNextE, 0)
+				e = readFileEBR(f, err)
+			}
+			x++
+
+		}
+
+	}
+
+}
+
+func ParticionLogica(path string, name string, tipo byte, fit byte, unit byte, size int64) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	auxSize := verifySize(unit, size)
+	auxName := converNameToByte(name)
+	fmt.Printf("%+v\n", auxSize)
+	f.Seek(0, 0)
+	m := readFileDisk(f, err)
+	var ebr structs_lwh.EBR
+	var checkPartition bool = false
+
+	for i := 0; i < 4; i++ {
+		if m.Partition[i].PartName == auxName {
+			checkPartition = true
+			break
+		} else if m.Partition[i].PartType == 'E' {
+			f.Seek(int64(m.Partition[i].PartStart), 0)
+			ebr = readFileEBR(f, err)
+			pos, _ := f.Seek(0, os.SEEK_CUR)
+			fmt.Println(pos)
+			eSize := binary.Size(ebr)
+			fmt.Println(eSize)
+			for eSize != 0 && pos < int64(m.Partition[i].PartSize)+int64(m.Partition[i].PartStart) {
+				if ebr.PartNameE == auxName {
+					checkPartition = true
+				}
+				if ebr.PartNextE == -1 {
+					break
+				} else {
+					f.Seek(ebr.PartNextE, 0)
+					ebr = readFileEBR(f, err)
+				}
+			}
+		}
+	}
+	if !checkPartition {
+		var index int = -1
+
+		for i := 0; i < 4; i++ {
+			if m.Partition[i].PartType == 'E' {
+				index = i
+				break
+			}
+		}
+		if index == -1 {
+			fmt.Println("EL ENUNCIADO INDICA QUE PARA LOGICA SE NECESITA UNA EXTENDIDA :)")
+		}
+		f.Seek(m.Partition[index].PartStart, 0)
+		ebr = readFileEBR(f, err)
+		if ebr.PartSizeE == 0 {
+			if m.Partition[index].PartSize < auxSize {
+				fmt.Println("El size de la particion logica debe ser menor a la extendida ya creada :p")
+			} else {
+				sizeM := binary.Size(ebr)
+				pos, _ := f.Seek(0, os.SEEK_CUR)
+				fmt.Println(pos)
+				ebr.PartStatusE = '0'
+				ebr.PartFitE = fit
+				ebr.PartStartE = pos - int64(sizeM)
+				ebr.PartSizeE = auxSize
+				ebr.PartNextE = -1
+				copy(ebr.PartNameE[:], name)
+				writeInLogicalPartition(path, m, ebr, index)
+				f.Seek(m.Partition[index].PartStart, 0)
+				auxMI := readFileEBR(f, err)
+				fmt.Println(auxMI)
+				fmt.Println("SE CREO LA PARTICION LOGICA")
+			}
+		} else {
+			pos, _ := f.Seek(0, os.SEEK_CUR)
+			for ebr.PartNextE != -1 && pos < (m.Partition[index].PartSize+m.Partition[index].PartStart) {
+				f.Seek(ebr.PartNextE, 0)
+				ebr = readFileEBR(f, err)
+
+			}
+			size1 := ebr.PartStartE + ebr.PartSizeE + auxSize
+			if size1 <= (m.Partition[index].PartSize + m.Partition[index].PartStart) {
+				ebr.PartNextE = ebr.PartStartE + ebr.PartSizeE
+
+				sizeM := binary.Size(ebr)
+				pos, _ := f.Seek(0, os.SEEK_CUR)
+				b := pos - int64(sizeM)
+				f.Seek(b, 0)
+
+				var binario bytes.Buffer
+
+				binary.Write(&binario, binary.BigEndian, &ebr)
+
+				writeNextBytes(f, binario.Bytes())
+
+				f.Seek(int64(ebr.PartStartE+ebr.PartSizeE), 0)
+				pos1, _ := f.Seek(0, os.SEEK_CUR)
+				ebr.PartFitE = fit
+				copy(ebr.PartNameE[:], name)
+				ebr.PartNextE = -1
+				ebr.PartSizeE = auxSize
+				ebr.PartStartE = pos1
+				ebr.PartStatusE = '0'
+
+				var binario2 bytes.Buffer
+
+				binary.Write(&binario2, binary.BigEndian, &ebr)
+
+				writeNextBytes(f, binario2.Bytes())
+
+				fmt.Println("SE CREO LA PARTICION LOGICA")
+			} else {
+				fmt.Println("LA PARTICION LOGICA SOBREPASA EL PESO DE LA EXTENDIDA")
+			}
+		}
+
+	} else {
+		fmt.Println("LA PARTICION LOGICA CON ESTE NOMBRE YA HA SIDO CREADA", name)
+	}
+}
+
 // FirstFit Crear el primer ajuste
-func FirstFit(m structs_lwh.MBR, size int32) int {
+func FirstFit(m structs_lwh.MBR, size int64) int {
 	var check bool = false
 	fmt.Println(check)
 	var indexSelect int = 0
@@ -360,7 +543,7 @@ func FirstFit(m structs_lwh.MBR, size int32) int {
 }
 
 // BestFit crear el mejor ajuste
-func BestFit(m structs_lwh.MBR, size int32) int {
+func BestFit(m structs_lwh.MBR, size int64) int {
 	var check bool = false
 	var indexSelect int = 0
 	for i := 0; i < 4; i++ {
@@ -378,7 +561,7 @@ func BestFit(m structs_lwh.MBR, size int32) int {
 }
 
 // WorstFit el peor ajuste :(
-func WorstFit(m structs_lwh.MBR, size int32) int {
+func WorstFit(m structs_lwh.MBR, size int64) int {
 	var check bool = false
 	var indexSelect int = 0
 	for i := 0; i < 4; i++ {
@@ -429,34 +612,47 @@ func converNameToByte(name string) [16]byte {
 	return auxName
 }
 
-func writeInPrimaryPartition(path string, m structs_lwh.MBR, index int) {
-	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+func writeInLogicalPartition(path string, m structs_lwh.MBR, e structs_lwh.EBR, index int) {
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	//var firstPos byte = '1'
 
 	//start := &firstPos
 	//struC := &m
-	f.Seek(int64(m.MbrSize), int(m.Partition[index].PartStart))
+	f.Seek(int64(m.Partition[index].PartStart), 0)
 	var binario bytes.Buffer
 
-	binary.Write(&binario, binary.BigEndian, &m)
+	binary.Write(&binario, binary.BigEndian, &e)
 
 	writeNextBytes(f, binario.Bytes())
 }
 
 func writeInExtenderPartition(path string, m structs_lwh.MBR, e structs_lwh.EBR, index int) {
-	f, err := os.OpenFile(path, os.O_RDWR, 0666)
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	f.Seek(int64(m.MbrSize), int(m.Partition[index].PartStart))
+	f.Seek(0, 0)
+
 	var binario bytes.Buffer
 
 	binary.Write(&binario, binary.BigEndian, &m)
-	binary.Write(&binario, binary.BigEndian, &e)
 
 	writeNextBytes(f, binario.Bytes())
+
+	f.Seek(m.Partition[index].PartStart, 0)
+
+	fmt.Println(strconv.Itoa(int(m.Partition[index].PartStart)))
+
+	var binario2 bytes.Buffer
+
+	binary.Write(&binario2, binary.BigEndian, &e)
+
+	writeNextBytes(f, binario2.Bytes())
+}
+
+func writeInExtenderPartitionIfExist(path string, m structs_lwh.MBR, e structs_lwh.EBR, index int, fit byte, auxSize int32) {
 
 }
