7b7f553df0cf349b4b801ed5056841644f6b2e86
dani3l8200 <dani3l25948@gmail.com>
2020-09-01 20:19:09 -0600

    se creo el struct para el mount

---
+++ structs_lwh/mount.go
@@ -0,0 +1,43 @@
+package structs_lwh
+
+// MountDisk estructura donde montará una partición del disco en el sistema.
+type MountDisk struct {
+	ID   string
+	Path string
+	Name string
+}
+
+//FMountDisk Inicializa la structura mount
+func (mount *MountDisk) FMountDisk(ID string, Path string, Name string) MountDisk {
+	return MountDisk{ID: ID, Path: Path, Name: Name}
+}
+
+//GetID retorna le id generado automaticamente
+func (mount *MountDisk) GetID() string {
+	return mount.ID
+}
+
+//SetID edita algun id, pero es opcional xd
+func (mount *MountDisk) SetID(ID string) {
+	mount.ID = ID
+}
+
+//GetPath retornal el directorio introducido
+func (mount *MountDisk) GetPath() string {
+	return mount.Path
+}
+
+//SetPath edita algun directorio, si lo fuera necesario
+func (mount *MountDisk) SetPath(Path string) {
+	mount.Path = Path
+}
+
+//GetName retorna el nombre del mount
+func (mount *MountDisk) GetName() string {
+	return mount.Name
+}
+
+//SetName edita algun nombre del mount, si fuera necesario
+func (mount *MountDisk) SetName(Name string) {
+	mount.Name = Name
+}
